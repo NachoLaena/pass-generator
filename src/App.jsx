51c761fb7e5cf99d973fd9f22156f5290e1b7e7a
Ignacio Laena <95891241+NachoLaena@users.noreply.github.com>
2023-10-04 10:40:02 -0300

    add style modification and context

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { PassGenProvider } from './context/PassGenContext'
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-    </>
+    <main className='h-screen flex justify-center items-center'>
+      <div className='flex p-4 flex-col items-center'>
+        <h1 className='font-bold text-3xl'>Password Generator</h1>
+        <p className='m-3'>What do you want to include?</p>
+        <PassGenProvider></PassGenProvider>
+        <p className='m-3'>Your password:</p>
+      </div>
+    </main>
   )
 }
 
