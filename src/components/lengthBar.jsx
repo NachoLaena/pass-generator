8b11666fa09dcd784676b07df8474b740ccefb2a
Ignacio Laena <95891241+NachoLaena@users.noreply.github.com>
2023-10-05 20:00:59 -0300

    ADD lengthBar component

---
+++ src/components/lengthBar.jsx
@@ -0,0 +1,23 @@
+import "./lengthBar.css";
+import { useContext } from "react";
+import { PassGenContext } from "../context/PassGenContext";
+
+export const LengthBar = () => {
+  const { lengthBar, setLengthBar } = useContext(PassGenContext);
+
+  const labelOnChange = (e) => {
+    setLengthBar(e.target.value);
+  };
+
+  return (
+    <div className="flex flex-col items-center mt-3">
+      <div className="flex items-center mb-2">
+        <label htmlFor="length">Length:</label>
+        <div className="flex items-center justify-center ml-1 h-8 w-8 border-2 border-[var(--dark-purple)] rounded font-bold">
+          {lengthBar}
+        </div>
+      </div>
+      <input type="range" min="6" max="30" step="1" onChange={labelOnChange} />
+    </div>
+  );
+};
